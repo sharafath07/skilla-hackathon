03c240c7695447ead7a0f2baac78f44e53c58873
Sharafath_V <142240872+sharafath07@users.noreply.github.com>
2025-09-21 05:52:59 +0000

    one

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()] ,
-    base: "/skilla-hackathon"
+    base: process.env.VITE_BASE_PATH || "/skilla-hackathon"
 })
